03e7117e24a01ccba8b10210d79624707b075992
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2022-12-11 21:26:08 +0100

    day11

---
+++ day11/part1.py
@@ -0,0 +1,58 @@
+import argparse
+import os.path
+
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+# NOTE: paste test text here
+INPUT_S = '''\
+
+'''
+EXPECTED = 1
+
+
+def compute(input_s: str) -> int:
+    q = r = s = 0
+    for dir in input_s.strip().split(','):
+        match dir:
+            # @formatter:off
+            case 'n':  s += 1; r -= 1
+            case 'ne': q += 1; r -= 1
+            case 'se': q += 1; s -= 1
+            case 's':  r += 1; s -= 1
+            case 'sw': r += 1; q -= 1
+            case 'nw': s += 1; q -= 1
+            # @formatter:on
+    return (abs(q) + abs(r) + abs(s)) // 2
+
+
+@pytest.mark.solved
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            ('ne,ne,ne', 3),
+            ('ne,ne,sw,sw', 0),
+            ('ne,ne,s,s', 2),
+            ('se,sw,se,sw,sw', 3),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
