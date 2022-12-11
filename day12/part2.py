40cbfb2db5017a8ecdfb54c7b205226abf8e6e8d
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2022-12-11 22:42:14 +0100

    day12
    added show_graph to support

---
+++ day12/part2.py
@@ -0,0 +1,59 @@
+import argparse
+import itertools
+import os.path
+
+import networkx as nx
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+# NOTE: paste test text here
+INPUT_S = '''\
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
+'''
+EXPECTED = 2
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    G = nx.Graph()
+    for line in lines:
+        line = line.replace(',', '')
+        _from, _, *_to = line.split()
+        G.add_edges_from(itertools.product((_from,), _to))
+    # support.show_graph(G)
+    return sum(1 for _ in nx.connected_components(G))
+
+
+@pytest.mark.solved
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
--- support/support.py
+++ support/support.py
@@ -12,6 +12,9 @@ import urllib.request
 from pathlib import Path
 from typing import Generator
 
+import networkx as nx
+from matplotlib import pyplot as plt
+
 HERE = os.path.dirname(os.path.abspath(__file__))
 ENV_FILE = os.path.join(HERE, '../../.env')
 
@@ -224,3 +227,8 @@ class Direction4(enum.Enum):
 
     def apply(self, x: int, y: int, *, n: int = 1) -> tuple[int, int]:
         return self.x * n + x, self.y * n + y
+
+
+def show_graph(G):
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.show()
